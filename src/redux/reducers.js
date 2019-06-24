97495646ac8ec692df14cd87aef51e5dba82e99d
AliceJuan <juanyanni@163.com>
2019-06-24 20:46:04 +0800

    use localStorage to cache data to prevent "undefined" after refresh

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -4,17 +4,17 @@ import * as localStorage from "../utils/localStorage"
 
 //需要存储的初始状态数据
 const initialState = {
-	city: {'cname': '北京'},
+	city: localStorage.getStore('city') || {'cname': '北京'},
 	film: {
-		filmId: '',
-		filmName: ''
+		filmId: localStorage.getStore('filmInfo').filmId,
+		filmName: localStorage.getStore('filmInfo').filmName
 	},
 	cinema: {
-		cinemaId: '',
-		cinemaName: ''
+		cinemaId: localStorage.getStore('cinemaInfo').cinemaId,
+		cinemaName: localStorage.getStore('cinemaInfo').cinemaName
 	},
-	video: {},
-	seat: [],
+	video: localStorage.getStore('video') || {},
+	seat: localStorage.getStore('seat') || [],
 	place: '/msite',
 	user: {}
 };
@@ -32,7 +32,7 @@ function place(place = initialState.place, action) {
 function city(city = initialState.city, action) {
 	switch (action.type) {
 		case types.UPDATE_CITY:
-			localStorage.setStore(action.city);
+			localStorage.setStore('city', action.city);
 			return action.city;
 		default:
 			return city;
@@ -42,6 +42,7 @@ function city(city = initialState.city, action) {
 function film(film = initialState.film, action){
 	switch (action.type) {
 		case types.SAVE_FILM_INFO:
+			localStorage.setStore('filmInfo', action.filmInfo);
 			return action.filmInfo;
 		case types.CHANGE_FILM_INFO:
 			return action.filmInfo;
@@ -53,6 +54,7 @@ function film(film = initialState.film, action){
 function cinema(cinema = initialState.cinema, action){
 	switch (action.type) {
 		case types.SAVE_CINEMA_INFO:
+			localStorage.setStore('cinemaInfo', action.cinemaInfo);
 			return action.cinemaInfo;
 		default:
 			return cinema;
@@ -62,6 +64,7 @@ function cinema(cinema = initialState.cinema, action){
 function video(video = initialState.video, action){
 	switch (action.type) {
 		case types.SAVE_VIDEO_TIME:
+			localStorage.setStore('video', action.time);
 			return action.time;
 		default:
 			return video;
@@ -75,39 +78,12 @@ function seat(seat=initialState.seat, action){
 //		case types.DELETE_SEAT:
 //			return action.seatInfo;
 		case types.SAVE_SEAT:
+			localStorage.setStore('seat', action.seat);
 			return action.seat;
 		default:
 			return seat;
 	}
 }
-
-
-//修改当前歌曲
-//function song(song = initialState.song, action) {
-//switch (action.type) {
-//  case ActionTypes.CHANGE_SONG:
-//    localStorage.setCurrentSong(action.song);
-//    return action.song;
-//  default:
-//    return song;
-//}
-//}
-
-//添加或移除歌曲
-//function songs(songs = initialState.songs, action) {
-//switch (action.type) {
-//  case ActionTypes.SET_SONGS:
-//    localStorage.setSongs(action.songs);
-//    return action.songs;
-//  case ActionTypes.REMOVE_SONG_FROM_LIST:
-//    let newSongs = songs.filter(song => song.id !== action.id);
-//    localStorage.setSongs(newSongs);
-//    return newSongs;
-//  default:
-//    return songs;
-//}
-//}
-
 
 //合并Reducer
 const reducer = combineReducers({
